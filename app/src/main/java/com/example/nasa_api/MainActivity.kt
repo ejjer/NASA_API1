76a5921d39f1132b8b105171c1439ded4f792c09
ThinkPad T470 <belkoegor1@gmail.com>
2022-07-06 21:43:35 +0300

    обработка кликов

--- app/src/main/java/com/example/nasa_api/MainActivity.kt
+++ app/src/main/java/com/example/nasa_api/MainActivity.kt
@@ -5,7 +5,14 @@ import android.os.Bundle
 import com.example.nasa_api.databinding.ActivityMainBinding
 import com.example.nasa_api.view.picture.PictureOfTheDayFragment
 
+const val GreenTheme  = 1
+const val BlueTheme  = 2
+
+
 class MainActivity : AppCompatActivity() {
+
+    private val KEY_SP = "sp"
+    private val KEY_CURRENT_THEME = "current_theme"
 
 
     private lateinit var binding: ActivityMainBinding
@@ -26,5 +33,26 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
 
+    }
+
+    fun setCurrentTheme(currentTheme: Int) {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_CURRENT_THEME, currentTheme)
+        editor.apply()
+    }
+
+    fun getCurrentTheme(): Int {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_CURRENT_THEME, -1)
+    }
+
+    private fun getRealStyle(currentTheme: Int): Int {
+        return when (currentTheme) {
+            BlueTheme -> R.style.BlueTheme
+            GreenTheme -> R.style.GreenTheme
+
+            else -> 0
+        }
     }
 }
