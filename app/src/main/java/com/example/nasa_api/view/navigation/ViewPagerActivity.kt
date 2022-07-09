c8c1e507780c5f2ea25962c5f6b6e063b949bb72
ThinkPad T470 <belkoegor1@gmail.com>
2022-07-09 21:34:43 +0300

    viewPager2

--- app/src/main/java/com/example/nasa_api/view/navigation/ViewPagerActivity.kt
+++ app/src/main/java/com/example/nasa_api/view/navigation/ViewPagerActivity.kt
@@ -3,6 +3,8 @@ package com.example.nasa_api.view.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nasa_api.databinding.ActivityViewPagerBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class ViewPagerActivity : AppCompatActivity() {
@@ -15,12 +17,31 @@ class ViewPagerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityViewPagerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.viewPager.adapter=ViewPagerAdapter(supportFragmentManager)
-        binding.tabLayout.setupWithViewPager(binding.viewPager)
+        binding.viewPager.adapter=ViewPagerAdapter2(this)
+        bindTabLayout()
     }
 
+    private fun bindTabLayout() {
+        TabLayoutMediator(
+            binding.tabLayout,
+            binding.viewPager,
+            object : TabLayoutMediator.TabConfigurationStrategy {
+                override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+                    tab.text = when (position) {
+                        0 -> {
+                            "Earth"
+                        }
+                        1 -> {
+                            "Mars"
+                        }
 
-
+                        else -> {
+                            "System"
+                        }
+                    }
+                }
+            }).attach()
+    }
 
 
 }
---
+++ app/src/main/java/com/example/nasa_api/view/navigation/ViewPagerAdapter2.kt
@@ -0,0 +1,20 @@
+package com.example.nasa_api.view.navigation
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter2(fa:FragmentActivity):FragmentStateAdapter(fa) {
+
+
+    private val fragments = arrayOf(EarthFragment(), MarsFragment(), SystemFragment())
+
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
