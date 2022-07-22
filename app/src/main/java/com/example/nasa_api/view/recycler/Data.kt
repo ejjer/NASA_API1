1244126df13480149ec5518ac25966cc21f4736c
ThinkPad T470 <belkoegor1@gmail.com>
2022-07-22 11:58:55 +0300

    onBindViewHolder

--- app/src/main/java/com/example/nasa_api/view/recycler/Data.kt
+++ app/src/main/java/com/example/nasa_api/view/recycler/Data.kt
@@ -4,6 +4,6 @@ const val TYPE_EARTH=  1
 const val TYPE_MARS=  2
 const val TYPE_HEADER=  3
 
-data class Data(val someText: String = "Text", val someDescription:String = "Description",
+data class Data(val name: String = "Text", val someDescription:String = "Description",
                 val type:Int = TYPE_MARS) {
 }
--- app/src/main/java/com/example/nasa_api/view/recycler/RecyclerAdapter.kt
+++ app/src/main/java/com/example/nasa_api/view/recycler/RecyclerAdapter.kt
@@ -9,25 +9,29 @@ import com.example.nasa_api.databinding.ActivityRecyclerItemHeaderBinding
 import com.example.nasa_api.databinding.ActivityRecyclerItemMarsBinding
 
 
-class RecyclerAdapter(val listData:List<Data> ):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecyclerAdapter(val listData: List<Data>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun getItemViewType(position: Int): Int {
         return listData[position].type
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
-        return  when(viewType){
-            TYPE_EARTH->{
-                val binding = ActivityRecyclerItemEarthBinding.inflate(LayoutInflater.from(parent.context))
+        return when (viewType) {
+            TYPE_EARTH -> {
+                val binding =
+                    ActivityRecyclerItemEarthBinding.inflate(LayoutInflater.from(parent.context))
                 EarthViewHolder(binding)
             }
-            TYPE_MARS->{
-                val binding = ActivityRecyclerItemMarsBinding.inflate(LayoutInflater.from(parent.context))
+            TYPE_MARS -> {
+                val binding =
+                    ActivityRecyclerItemMarsBinding.inflate(LayoutInflater.from(parent.context))
                 MarsViewHolder(binding)
 
             }
-            else->{
-                val binding = ActivityRecyclerItemHeaderBinding.inflate(LayoutInflater.from(parent.context))
+            else -> {
+                val binding =
+                    ActivityRecyclerItemHeaderBinding.inflate(LayoutInflater.from(parent.context))
                 HeaderViewHolder(binding)
             }
         }
@@ -36,18 +40,48 @@ class RecyclerAdapter(val listData:List<Data> ):RecyclerView.Adapter<RecyclerVie
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        //TODO("Not yet implemented")
+        when (getItemViewType(position)) {
+            TYPE_EARTH -> {
+                (holder as EarthViewHolder).bind(listData[position])
+
+            }
+            TYPE_MARS -> {
+                (holder as MarsViewHolder).bind(listData[position])
+
+
+            }
+            else -> {
+                (holder as HeaderViewHolder).bind(listData[position])
+
+
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return listData.size
     }
 
-    class MarsViewHolder(val binding : ActivityRecyclerItemMarsBinding):RecyclerView.ViewHolder(binding.root)
+    class MarsViewHolder(val binding: ActivityRecyclerItemMarsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Data) {
+            binding.name.text = data.name
+        }
+    }
 
-    class EarthViewHolder(val binding : ActivityRecyclerItemEarthBinding):RecyclerView.ViewHolder(binding.root)
+    class EarthViewHolder(val binding: ActivityRecyclerItemEarthBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Data) {
+            binding.name.text = data.name
+        }
+    }
 
 
-    class HeaderViewHolder(val binding : ActivityRecyclerItemHeaderBinding):RecyclerView.ViewHolder(binding.root)
+    class HeaderViewHolder(val binding: ActivityRecyclerItemHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Data) {
+            binding.name.text = data.name
+        }
+    }
 
 }
